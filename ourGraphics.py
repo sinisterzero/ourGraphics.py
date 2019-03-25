62174c9621290da83bf7d81f7a76d1b3b7928853
csPython <cspython@AD25TF04NGF1J-85.local>
2019-03-25 10:51:15 -0400

    background color added

--- ourGraphics.py
+++ ourGraphics.py
@@ -4,3 +4,7 @@ import random
 winX = 800
 wind = GraphWin("ourGraphics", winX, winX)
 wind.setCoords(0, 0, 800, 800)
+ 
+bg = Rectangle(Point(0,0), Point(winX, winX))
+bg.setFill(color_rgb(40,40,40))
+bg.draw(wind)
