7b0e62b60dea532e803603fe2e18a3d803382e07
csPython <cspython@AD25TF04NGF1J-85.local>
2019-04-01 10:51:39 -0400

    shape duplicate on each other

--- ourGraphics.py
+++ ourGraphics.py
@@ -1,10 +1,16 @@
 from graphics import*
-import random
-
+import random     
 def trapa(x,y,xe):
     trap = Polygon(Point(x,y),Point(x + xe*4,y),Point(x +xe*3,y+xe),Point(x + xe,y+xe))
     trap.setFill(color_rgb(200,200,200))
     trap.draw(wind)
+def stor(x,y):
+    sq = Rectangle(Point(x,y), Point(x+300,y+150))
+    sq.setFill(color_rgb(255,255,255))
+    sq.draw(wind)
+def story(x,y,xe):
+    trapa(x,y,xe)
+    stor(x+xe,y+xe)
 winX = 800
 wind = GraphWin("ourGraphics", winX, winX)
 wind.setCoords(0, 0, 800, 800)
@@ -13,4 +19,11 @@ bg = Rectangle(Point(0,0), Point(winX, winX))
 bg.setFill(color_rgb(255,255,255))
 bg.draw(wind)
 
-trapa(50,100,150)
+x = 50
+y = 100
+xe = 150
+
+for i in range(2):
+    story(x,y,xe)
+    y += 300
+    xe -= 50
