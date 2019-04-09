f09c9255c76d2a28b88a6aa8833e0d3ea349355a
csPython <cspython@AD25TF04NGF1J-85.local>
2019-04-09 09:23:43 -0400

    layer repeat on top of each other

--- ourGraphics.py
+++ ourGraphics.py
@@ -4,13 +4,18 @@ def trapa(x,y,xe):
     trap = Polygon(Point(x,y),Point(x + xe*4,y),Point(x +xe*3,y+xe),Point(x + xe,y+xe))
     trap.setFill(color_rgb(200,200,200))
     trap.draw(wind)
-def stor(x,y):
-    sq = Rectangle(Point(x,y), Point(x+300,y+150))
+def stor(x,y,xe):
+    sq = Rectangle(Point(x,y), Point(x+xe*2,y+xe))
     sq.setFill(color_rgb(255,255,255))
     sq.draw(wind)
+def tria(x,y,xe):
+    tri = Polygon(Point(x,y),Point(x+xe*2,y),Point(x+xe,y+xe))
+    tri.setFill(color_rgb(255,255,255))
+    tri.draw(wind)
 def story(x,y,xe):
     trapa(x,y,xe)
-    stor(x+xe,y+xe)
+    stor(x+xe,y+xe,xe)
+    tria(x+xe,y,xe)
 winX = 800
 wind = GraphWin("ourGraphics", winX, winX)
 wind.setCoords(0, 0, 800, 800)
@@ -20,10 +25,11 @@ bg.setFill(color_rgb(255,255,255))
 bg.draw(wind)
 
 x = 50
-y = 100
+y = 0
 xe = 150
 
-for i in range(2):
+for i in range(4):
     story(x,y,xe)
-    y += 300
-    xe -= 50
+    y += 300-(50*i+1)
+    xe -= 25
+    x += 50
