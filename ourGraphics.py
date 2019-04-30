b61a2cc0ef1f8e9e5a25c73c9e85cb0969e86c5a
csPython <cspython@AD25TF04NGF1J-85.local>
2019-04-30 11:37:44 -0400

    added window and more detail

--- ourGraphics.py
+++ ourGraphics.py
@@ -1,21 +1,31 @@
 from graphics import*
 import random     
-def trapa(x,y,xe):
+def trapa(x,y,xe):#roof
     trap = Polygon(Point(x,y),Point(x + xe*4,y),Point(x +xe*3,y+xe),Point(x + xe,y+xe))
     trap.setFill(color_rgb(200,200,200))
     trap.draw(wind)
-def stor(x,y,xe):
+def stor(x,y,xe):#block on top roof
     sq = Rectangle(Point(x,y), Point(x+xe*2,y+xe))
     sq.setFill(color_rgb(255,255,255))
     sq.draw(wind)
-def tria(x,y,xe):
+def tria(x,y,xe):#triangle in the middle of the roof
     tri = Polygon(Point(x,y),Point(x+xe*2,y),Point(x+xe,y+xe))
     tri.setFill(color_rgb(255,255,255))
     tri.draw(wind)
-def story(x,y,xe):
+def story(x,y,xe,rt):#create one level
     trapa(x,y,xe)
     stor(x+xe,y+xe,xe)
     tria(x+xe,y,xe)
+    swindow(x,y,xe,rt)
+def window(x,y,xe):#window
+    rec = Rectangle(Point(x,y), Point(x+10,y+xe-10))
+    rec.setFill(color_rgb(0,0,0))
+    rec.draw(wind)
+def swindow(x,y,xe,rt):#make more window across the block
+    for i in range(rt):
+        window(x+(xe),(y-xe),xe)
+        x += 50
+    
 winX = 800
 wind = GraphWin("ourGraphics", winX, winX)
 wind.setCoords(0, 0, 800, 800)
@@ -24,12 +34,14 @@ bg = Rectangle(Point(0,0), Point(winX, winX))
 bg.setFill(color_rgb(255,255,255))
 bg.draw(wind)
 
-x = 50
+x = 100
 y = 0
 xe = 150
-
+windt = 7
+#create the tower
 for i in range(4):
-    story(x,y,xe)
+    story(x,y,xe,windt)
     y += 300-(50*i+1)
     xe -= 25
     x += 50
+    windt -= 1
